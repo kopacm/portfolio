f416cff4b1fe420968e1e9ab55bf597f9f906513
Miroslav Kopac <80456649+kopacm@users.noreply.github.com>
2025-01-06 13:22:18 +0100

    Update Find the rate of processed tickets for each type.sql

--- SQL Questions/Find the rate of processed tickets for each type.sql
+++ SQL Questions/Find the rate of processed tickets for each type.sql
@@ -1,4 +1,8 @@
+"Find the average total compensation based on employee titles and gender. Total compensation is calculated by adding both the salary and bonus of each employee. However, not every employee receives a bonus so disregard employees without bonuses in your calculation. Employee can receive more than one bonus.
+Output the employee title, gender (i.e., sex), along with the average total compensation."
+  
 SELECT
+  
 processed_tickets/all_tickets as rate_of_proccesed,
 type
 from(
